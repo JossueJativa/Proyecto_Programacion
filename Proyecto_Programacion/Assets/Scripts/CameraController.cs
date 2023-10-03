63b31019a3f9420d213703701883c1e6d2bab328
Jossue Jativa <jativatamayojossuedavid@gmail.com>
2023-10-03 17:22:40 -0500

    Seguimiento de la camara al personaje

--- Proyecto_Programacion/Assets/Scripts/CameraController.cs
+++ Proyecto_Programacion/Assets/Scripts/CameraController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform objetivo; // Aquí arrastra el objeto del personaje que quieres seguir
+    public Vector3 offset = new Vector3(0, 0, -10); // Ajusta la posición de la cámara en relación al personaje
+
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        potitionCamera();
+    }
+
+    void potitionCamera(){
+        if (objetivo != null)
+        {
+            // Obtén la posición actual del personaje
+            Vector3 posicionObjetivo = objetivo.position + offset;
+
+            // Interpola suavemente la posición de la cámara hacia la posición del personaje
+            transform.position = Vector3.Lerp(transform.position, posicionObjetivo, Time.deltaTime * 5f);
+        }
     }
 }
