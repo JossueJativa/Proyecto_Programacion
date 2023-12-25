d48e190efe205f8d48774a61c8cc73e6c6062006
Jossue <jativatamayojossuedavid@gmail.com>
2023-12-25 13:04:32 -0500

    Cambios a realizar

--- Proyecto_Programacion/Assets/Scripts/objectController.cs
+++ Proyecto_Programacion/Assets/Scripts/objectController.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class objectController : MonoBehaviour
+public class ObjectController : MonoBehaviour
 {
-    [SerializeField] private GameObject objectToSpawn;
-    [SerializeField] private int cantidad = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +14,5 @@ public class objectController : MonoBehaviour
     void Update()
     {
         
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.CompareTag("Player")) {
-            Destroy(gameObject);
-        }
     }
 }
